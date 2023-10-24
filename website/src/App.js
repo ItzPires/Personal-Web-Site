932b5696bdf63b8a5795c9e81a6c8bd2f12a6bbe
Samuel Pires <spires@ipn.pt>
2023-10-24 23:21:30 +0100

    Layout praticamente feito

--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import eu from './imgs/eu.png';
+import dei from './imgs/dei.png';
+import chavena from './imgs/chavena.png';
+import viseu from './imgs/viseu.png';
+import lua from './imgs/lua.png';
+import botanico from './imgs/botanico.jpg';
+import matrecos from './imgs/matrecos.png';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="row">
+      <div className="col-4 a">
+        <img src={chavena} alt="chavena" id="chavena" />
+        <img src={viseu} alt="viseu" id="viseu" />
+        <img src={dei} alt="dei" id="dei" />
+      </div>
+      <div className="col-4 a">
+        <img src={eu} alt="eu" className="img-responsive" />
+      </div>
+      <div className="col-4 a">
+        <img src={lua} alt="lua" id="lua" />
+        <img src={botanico} alt="botanico" id="botanico" />
+        <img src={matrecos} alt="matrecos" id="matrecos" />
+      </div>
     </div>
   );
 }
