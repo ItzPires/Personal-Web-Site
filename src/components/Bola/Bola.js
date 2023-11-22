f0344aae6dfc9e9f1f0db407bcebe93b6595f733
Samuel Pires <spires@ipn.pt>
2023-11-22 00:02:14 +0000

    Avoid middle column

--- src/components/Bola/Bola.js
+++ src/components/Bola/Bola.js
@@ -13,6 +13,13 @@ const BolaComponent = ({ topInicial, leftInicial }) => {
         const alturaBola = 50;
         const top = Math.floor(Math.random() * (alturaTela - alturaBola));
         const left = Math.floor(Math.random() * (larguraTela - larguraBola));
+
+        let largura3 = larguraTela / 3;
+
+        if (left > largura3 && left < largura3 * 2) {
+            return gerarCoordenadas();
+        }
+
         return { top, left };
     }
 
